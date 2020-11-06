28e30160bde1aa213651a4bb1db5d3ced5f64f40
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-06 10:32:24 +0000

    Updated files with 'repo_helper'.

--- README.rst
+++ README.rst
@@ -29,7 +29,7 @@ The pertinent changes from Python 3.6 to Python 3.9 are:
 	:widths: 10 90
 
 	* - Tests
-	  - |travis| |actions_windows| |actions_macos| |coveralls| |codefactor|
+	  - |travis| |actions_windows| |actions_macos| |coveralls| |codefactor| |pre_commit_ci|
 	* - PyPI
 	  - |pypi-version| |supported-versions| |supported-implementations| |wheel|
 	* - Activity
@@ -100,6 +100,10 @@ The pertinent changes from Python 3.6 to Python 3.9 are:
 .. |pre_commit| image:: https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white
 	:target: https://github.com/pre-commit/pre-commit
 	:alt: pre-commit
+
+.. |pre_commit_ci| image:: https://results.pre-commit.ci/badge/github/domdfcoding/pprint36/master.svg
+	:target: https://results.pre-commit.ci/latest/github/domdfcoding/pprint36/master
+	:alt: pre-commit.ci status
 
 .. end shields
 
--- __pkginfo__.py
+++ __pkginfo__.py
@@ -17,22 +17,9 @@ import pathlib
 __all__ = [
 		"__copyright__",
 		"__version__",
-		"modname",
-		"pypi_name",
-		"__license__",
-		"__author__",
-		"short_desc",
-		"author",
-		"author_email",
-		"github_username",
-		"web",
-		"github_url",
 		"repo_root",
 		"install_requires",
 		"extras_require",
-		"project_urls",
-
-		"import_name",
 		]
 
 __copyright__ = """
@@ -40,23 +27,7 @@ __copyright__ = """
 """
 
 __version__ = "3.9.0.2"
-modname = "pprint36"
-pypi_name = "pprint36"
-import_name = "pprint36"
-__license__ = "PSFLicense"
-short_desc = 'Backport of pprint from Python 3.9 to Python 3.6-3.8'
-__author__ = author = 'Python Software Foundation'
-author_email = 'dominic@davis-foster.co.uk'
-github_username = "domdfcoding"
-web = github_url = "https://github.com/domdfcoding/pprint36"
+
 repo_root = pathlib.Path(__file__).parent
 install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
 extras_require = {'all': []}
-
-
-
-project_urls = {
-		"Documentation": "https://pprint36.readthedocs.io",
-		"Issue Tracker": f"{github_url}/issues",
-		"Source Code": github_url,
-		}
